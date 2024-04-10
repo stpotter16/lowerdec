b0bc6c88dd2d0691cc647d97662e4dcb239b4fd0
stpotter16 <stpotter1642@gmail.com>
2024-04-09 21:51:21 -0400

    Add structure for templates and styles

--- main.go
+++ main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+    "embed"
     "fmt"
+    "html/template"
+    "io/fs"
     "net/http"
 )
 
+var (
+    //go:embed  static/style.css
+    css embed.FS
+
+    //go:embed all:templates/*
+    templatesFS embed.FS
+
+    html *template.Template
+)
+
+func parseTemplates(templates fs.FS) *template.Template {
+    parsed := template.Must(template.New("").ParseFS(templates, "templates/*.html"))
+    return parsed
+}
+
 func main() {
+    html = parseTemplates(templatesFS)
+
     http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Welcome!")
+    })
+    http.HandleFunc("/static/style.css", func (w http.ResponseWriter, r *http.Request) {
+        http.FileServer(http.FS(css))
     })
 
     http.ListenAndServe(":8080", nil)
