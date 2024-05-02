bd4cf06861857f5cc175a5d86387540bfeafa6f5
stpotter16 <stpotter1642@gmail.com>
2024-05-01 21:27:03 -0400

    Add database interaction

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+    "context"
+    "database/sql"
     "embed"
+    "fmt"
     "html/template"
-    "io/fs"
     "log"
     "net/http"
+    "os"
+    "os/signal"
+    "syscall"
+
+    _ "github.com/mattn/go-sqlite3"
+    "github.com/google/uuid"
 )
 
 var (
@@ -19,17 +27,34 @@ var (
     favicon embed.FS
     //go:embed static/assets/logo.png
     navLogo embed.FS
-
-    html *template.Template
 )
 
-func parseTemplates(templates fs.FS) *template.Template {
-    parsed := template.Must(template.New("").ParseFS(templates, "templates/*.html"))
-    return parsed
-}
+const DB_PATH = "DB_PATH"
 
-func main() {
-    html = parseTemplates(templatesFS)
+func run() error {
+    ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM)
+    defer stop()
+
+    dbPath, present := os.LookupEnv(DB_PATH)
+
+    if !present {
+        return fmt.Errorf("%s not set", DB_PATH)
+    }
+
+    db, err := sql.Open("sqlite3", dbPath)
+    if err != nil {
+        return err
+    }
+    defer db.Close()
+
+    if _, err := db.Exec(`CREATE TABLE IF NOT EXISTS signups (userId STRING PRIMARY KEY, name STRING, email STRING);`); err != nil {
+        return fmt.Errorf("Cannot create table: %w", err)
+    }
+
+    html, err := template.New("").ParseFS(templatesFS, "templates/*.html")
+    if err != nil {
+        return err
+    }
 
     http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
         log.Printf("%s: %s", r.Method, r.URL.Path)
@@ -43,6 +68,11 @@ func main() {
         }
         log.Printf("%s: %s", "full name", r.FormValue("name"))
         log.Printf("%s: %s", "email", r.FormValue("email"))
+        userId := uuid.New()
+        if _, err := db.Exec(`INSERT INTO signups (userId, name, email) VALUES (?, ?, ?)`, userId.String(), r.FormValue("name"), r.FormValue("email")); err != nil {
+            // TODO - should return an error
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+        }
         html.ExecuteTemplate(w, "start.html", struct{ Success bool }{true})
     })
     http.HandleFunc("/static/style.css", func (w http.ResponseWriter, r *http.Request) {
@@ -61,6 +91,18 @@ func main() {
         handler.ServeHTTP(w, r)
     })
 
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    http.ListenAndServe(":8080", nil)
+
+    <-ctx.Done()
+    log.Print("Received termination signal. Shutting down")
+
+    return nil
+}
+
+func main() {
+    if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
 
